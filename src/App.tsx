d4f4f8a923f9500a70ab57140524007afe61f4ba
Andrei S <svonik@gmail.com>
2026-03-17 01:16:46 +0200

    perf: add React.lazy loading for all page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
+import { lazy, Suspense } from 'react';
 import { HashRouter, Routes, Route, Navigate } from 'react-router';
 import { isDisclaimerAccepted, getCalibration } from './modules/storage';
 import { DisclaimerGuard } from './guards/DisclaimerGuard';
 import { CalibrationGuard } from './guards/CalibrationGuard';
 import { GameSettingsGuard } from './guards/GameSettingsGuard';
 import { Layout } from './components/Layout';
-import { OnboardingWizard } from './pages/OnboardingWizard';
-import { GameSelectPage } from './pages/GameSelectPage';
-import { SettingsPage } from './pages/SettingsPage';
-import { GamePage } from './pages/GamePage';
-import { StatsPage } from './pages/StatsPage';
-import { ProgressPage } from './pages/ProgressPage';
-import { SettingsHub } from './pages/SettingsHub';
-import { ModeSelectPage } from './pages/ModeSelectPage';
-import { TrainingSettingsPage } from './pages/TrainingSettingsPage';
-import { TrainingPlayPage } from './pages/TrainingPlayPage';
-import { TrainingSummaryPage } from './pages/TrainingSummaryPage';
+
+const OnboardingWizard = lazy(() => import('./pages/OnboardingWizard').then(m => ({ default: m.OnboardingWizard })));
+const GameSelectPage = lazy(() => import('./pages/GameSelectPage').then(m => ({ default: m.GameSelectPage })));
+const SettingsPage = lazy(() => import('./pages/SettingsPage').then(m => ({ default: m.SettingsPage })));
+const GamePage = lazy(() => import('./pages/GamePage').then(m => ({ default: m.GamePage })));
+const StatsPage = lazy(() => import('./pages/StatsPage').then(m => ({ default: m.StatsPage })));
+const ProgressPage = lazy(() => import('./pages/ProgressPage').then(m => ({ default: m.ProgressPage })));
+const SettingsHub = lazy(() => import('./pages/SettingsHub').then(m => ({ default: m.SettingsHub })));
+const ModeSelectPage = lazy(() => import('./pages/ModeSelectPage').then(m => ({ default: m.ModeSelectPage })));
+const TrainingSettingsPage = lazy(() => import('./pages/TrainingSettingsPage').then(m => ({ default: m.TrainingSettingsPage })));
+const TrainingPlayPage = lazy(() => import('./pages/TrainingPlayPage').then(m => ({ default: m.TrainingPlayPage })));
+const TrainingSummaryPage = lazy(() => import('./pages/TrainingSummaryPage').then(m => ({ default: m.TrainingSummaryPage })));
 
 function isOnboardingComplete() {
     return isDisclaimerAccepted() && getCalibration().suppression_passed;
@@ -23,40 +25,42 @@ function isOnboardingComplete() {
 function App() {
     return (
         <HashRouter>
-            <Routes>
-                <Route path="/onboarding" element={<OnboardingWizard />} />
-                <Route path="/games/:gameId/play" element={
-                    <DisclaimerGuard><CalibrationGuard><GameSettingsGuard><GamePage /></GameSettingsGuard></CalibrationGuard></DisclaimerGuard>
-                } />
-                <Route path="/training/play" element={
-                    <DisclaimerGuard><CalibrationGuard><TrainingPlayPage /></CalibrationGuard></DisclaimerGuard>
-                } />
-                <Route path="/*" element={
-                    <Layout>
-                        <Routes>
-                            <Route path="/" element={
-                                <Navigate to={isOnboardingComplete() ? '/mode-select' : '/onboarding'} replace />
-                            } />
-                            <Route path="/mode-select" element={
-                                <DisclaimerGuard><CalibrationGuard><ModeSelectPage /></CalibrationGuard></DisclaimerGuard>
-                            } />
-                            <Route path="/games" element={
-                                <DisclaimerGuard><CalibrationGuard><GameSelectPage /></CalibrationGuard></DisclaimerGuard>
-                            } />
-                            <Route path="/games/:gameId/settings" element={
-                                <DisclaimerGuard><CalibrationGuard><SettingsPage /></CalibrationGuard></DisclaimerGuard>
-                            } />
-                            <Route path="/games/:gameId/stats" element={<StatsPage />} />
-                            <Route path="/training/settings" element={
-                                <DisclaimerGuard><CalibrationGuard><TrainingSettingsPage /></CalibrationGuard></DisclaimerGuard>
-                            } />
-                            <Route path="/training/summary" element={<TrainingSummaryPage />} />
-                            <Route path="/progress" element={<ProgressPage />} />
-                            <Route path="/settings" element={<SettingsHub />} />
-                        </Routes>
-                    </Layout>
-                } />
-            </Routes>
+            <Suspense fallback={<div className="min-h-screen bg-[var(--bg)]" />}>
+                <Routes>
+                    <Route path="/onboarding" element={<OnboardingWizard />} />
+                    <Route path="/games/:gameId/play" element={
+                        <DisclaimerGuard><CalibrationGuard><GameSettingsGuard><GamePage /></GameSettingsGuard></CalibrationGuard></DisclaimerGuard>
+                    } />
+                    <Route path="/training/play" element={
+                        <DisclaimerGuard><CalibrationGuard><TrainingPlayPage /></CalibrationGuard></DisclaimerGuard>
+                    } />
+                    <Route path="/*" element={
+                        <Layout>
+                            <Routes>
+                                <Route path="/" element={
+                                    <Navigate to={isOnboardingComplete() ? '/mode-select' : '/onboarding'} replace />
+                                } />
+                                <Route path="/mode-select" element={
+                                    <DisclaimerGuard><CalibrationGuard><ModeSelectPage /></CalibrationGuard></DisclaimerGuard>
+                                } />
+                                <Route path="/games" element={
+                                    <DisclaimerGuard><CalibrationGuard><GameSelectPage /></CalibrationGuard></DisclaimerGuard>
+                                } />
+                                <Route path="/games/:gameId/settings" element={
+                                    <DisclaimerGuard><CalibrationGuard><SettingsPage /></CalibrationGuard></DisclaimerGuard>
+                                } />
+                                <Route path="/games/:gameId/stats" element={<StatsPage />} />
+                                <Route path="/training/settings" element={
+                                    <DisclaimerGuard><CalibrationGuard><TrainingSettingsPage /></CalibrationGuard></DisclaimerGuard>
+                                } />
+                                <Route path="/training/summary" element={<TrainingSummaryPage />} />
+                                <Route path="/progress" element={<ProgressPage />} />
+                                <Route path="/settings" element={<SettingsHub />} />
+                            </Routes>
+                        </Layout>
+                    } />
+                </Routes>
+            </Suspense>
         </HashRouter>
     );
 }
